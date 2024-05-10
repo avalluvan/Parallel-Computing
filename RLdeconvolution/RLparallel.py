6b7cc5d537737559a5a695f3c38b85558d787f61
avalluvan <62253557+avalluvan@users.noreply.github.com>
2024-05-09 22:26:52 -0700

    Working toy model update

--- RLdeconvolution/RLparallel.py
+++ RLdeconvolution/RLparallel.py
@@ -7,16 +7,25 @@ import h5py
 NUMROWS = 50
 NUMCOLS = 50
 
-MASTER = 0
-FROM_MASTER = 1  # Message tag
-FROM_WORKER = 2  # Message tag
+MASTER = 0      # Indicates master process
 
 def load_response_matrix(comm, start_row, end_row, filename='example.h5'):
     with h5py.File(filename, "r", driver="mpio", comm=comm) as f:
+        
         # Assuming the dataset name is "response_matrix"
         dataset = f["response_matrix"]
         R = dataset[start_row:end_row, :]
+
     return R
+
+def load_response_matrix_transpose(comm, start_col, end_col, filename='example.h5'):
+    with h5py.File(filename, "r", driver="mpio", comm=comm) as f:
+
+        # Assuming the dataset name is "response_matrix"
+        dataset = f["response_matrix"]
+        RT = dataset[:, start_col:end_col]
+
+    return RT
 
 def load_sky_model():
     # Load the sky model (M) with values from 1 to NUMCOLS
@@ -35,61 +44,79 @@ def main():
 
 # ****************************** MPI ******************************
 
-    # Calculate the indices in Rij that the node has to parse. My hunch is that calculating these scalars individually will be faster than the MPI send broadcast overhead
-    averow = NUMROWS // numtasks
-    extra_rows = NUMROWS % numtasks
-    start_row = taskid * averow
-    end_row = (taskid + 1) * averow if taskid < (numtasks - 1) else NUMROWS   # Provision to eventually remove criteria 1
-    # print(f"taskid {taskid}, endrow {end_row}")
+# **************************** Part I *****************************
 
-    epsilon_slice = np.zeros(end_row - start_row)
+    # print(f"taskid {taskid}, arr {arr}")
 
+    '''*************** Master ***************'''
     if taskid == MASTER:
         print(f"Hello from Master. Starting with {numtasks} threads.")
 
         # Initialise C vector. Only master requires full length.
-        C = np.zeros(NUMCOLS)
+        C = np.empty(NUMCOLS, dtype=np.float64)
 
         # Load sky model input
         M = load_sky_model()
 
+    '''*************** Worker ***************'''
+
+    if taskid > MASTER:
+        # Only separate if... clause for NON-MASTER in this toy model
+        # Initialise C vector to None. Only master requires full length.
+        C = None
+
+    '''**************** All *****************'''
+
+    # Broadcast M vector
     comm.Bcast([M, MPI.DOUBLE], root=MASTER)
+
+    # Calculate the indices in Rij that the process has to parse. My hunch is that calculating these scalars individually will be faster than the MPI send broadcast overhead
+    averow = NUMROWS // numtasks
+    extra_rows = NUMROWS % numtasks
+    start_row = taskid * averow
+    end_row = (taskid + 1) * averow if taskid < (numtasks - 1) else NUMROWS
+
+    epsilon_slice = np.zeros(end_row - start_row)
 
     # Calculate epsilon slice
     R = load_response_matrix(comm, start_row, end_row)
     epsilon_slice = np.dot(R, M)      # XXX: Can be GPU accelerated
 
-    # All gather epsilon slices
+    # All vector gather epsilon slices
     recvcounts = [averow] * (numtasks-1) + [averow + extra_rows]
     displacements = np.arange(numtasks) * averow
     comm.Allgatherv(epsilon_slice, [epsilon, recvcounts, displacements, MPI.DOUBLE])
 
+    # Sanity check: print epsilon
     if taskid == 0:
         print(epsilon)
         print()
 
-    # if taskid == MASTER:
-    #     for dest in range(1, numworkers + 1):
-    #         mtype = FROM_WORKER
-    #         offset_cols = comm.recv(source=dest, tag=mtype)
-    #         cols = comm.recv(source=dest, tag=mtype)
-    #         C[offset_cols : offset_cols + cols] = comm.recv(source=dest, tag=mtype)
-    #     print(C)
+# **************************** Part II *****************************
+    
+    # Calculate the indices in Rji that the process has to parse.
+    avecol = NUMCOLS // numtasks
+    extra_cols = NUMCOLS % numtasks
+    start_col = taskid * avecol
+    end_col = (taskid + 1) * avecol if taskid < (numtasks - 1) else NUMCOLS
 
-    # elif taskid > MASTER:
-    #     # Calculate C slice
-    #     for j in range(cols):
-    #         for i in range(NUMROWS):
-    #             C[j] += R[i, offset_cols + j] / epsilon[i]
+    # Calculate C slice
+    RT = load_response_matrix_transpose(comm, start_col, end_col)
+    C_slice = np.dot(RT.T, 1/epsilon)
 
-    #     # Send C slice back to master
-    #     comm.send(offset_cols, dest=MASTER, tag=FROM_WORKER)
-    #     comm.send(cols, dest=MASTER, tag=FROM_WORKER)
-    #     comm.send(C[:cols], dest=MASTER, tag=FROM_WORKER)
+    # All vector gather C slices
+    recvcounts = [avecol] * (numtasks-1) + [avecol + extra_cols]
+    displacements = np.arange(numtasks) * avecol
+    comm.Gatherv(C_slice, [C, recvcounts, displacements, MPI.DOUBLE], root=MASTER)
 
-    print("Almost Done\n")
+    # Sanity check: print C
+    if taskid == 0:
+        print(C)
+        print()
+        print("DONE")
+
+    # MPI Shutdown
     MPI.Finalize()
-    print("Done")
 
 if __name__ == "__main__":
     main()
