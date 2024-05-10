33fccfcbc983e09a227ae99af6694c4c567c00b5
avalluvan <62253557+avalluvan@users.noreply.github.com>
2024-05-09 21:10:11 -0700

    Working epsilon slice and gather

--- RLdeconvolution/RLparallel.py
+++ RLdeconvolution/RLparallel.py
@@ -1,3 +1,4 @@
+# Import third party libraries
 import numpy as np
 from mpi4py import MPI
 import h5py
@@ -10,12 +11,6 @@ MASTER = 0
 FROM_MASTER = 1  # Message tag
 FROM_WORKER = 2  # Message tag
 
-def init_response_matrix():
-    # Initialize the response matrix R
-    R = np.zeros((NUMROWS, NUMCOLS))
-    np.fill_diagonal(R, 2.0)  # Set diagonal elements to 2.0
-    return R
-
 def load_response_matrix(comm, start_row, end_row, filename='example.h5'):
     with h5py.File(filename, "r", driver="mpio", comm=comm) as f:
         # Assuming the dataset name is "response_matrix"
@@ -25,113 +20,73 @@ def load_response_matrix(comm, start_row, end_row, filename='example.h5'):
 
 def load_sky_model():
     # Load the sky model (M) with values from 1 to NUMCOLS
-    M = np.arange(1, NUMCOLS + 1)
+    M = np.arange(1, NUMCOLS + 1, dtype=np.float64)
     return M
 
 def main():
+    # Set up MPI
     comm = MPI.COMM_WORLD
     numtasks = comm.Get_size()
     taskid = comm.Get_rank()
 
-    if numtasks < 2:
-        print("Need at least two MPI tasks. Quitting...")
-        MPI.Abort(MPI.COMM_WORLD, 1)
-        exit(1)
+    # Initialise vectors required by all processes
+    M = np.empty(NUMCOLS, dtype=np.float64)     # Will be loaded and broadcasted by master. 
+    epsilon = np.zeros(NUMROWS)                 # Will be "All gatherv-ed".
 
-    numworkers = numtasks - 1
+# ****************************** MPI ******************************
 
-    epsilon = np.zeros(NUMROWS)
-    C = np.zeros(NUMCOLS)
+    # Calculate the indices in Rij that the node has to parse. My hunch is that calculating these scalars individually will be faster than the MPI send broadcast overhead
+    averow = NUMROWS // numtasks
+    start_row = taskid * averow
+    end_row = (taskid + 1) * averow if taskid < (numtasks - 1) else NUMROWS   # Provision to eventually remove criteria 1
+    # print(f"taskid {taskid}, endrow {end_row}")
+
+    epsilon_slice = np.zeros(end_row - start_row)
+
+    if taskid == MASTER:
+        print(f"Hello from Master. Starting with {numtasks} threads.")
+
+        # Initialise C vector. Only master requires full length.
+        C = np.zeros(NUMCOLS)
+
+        # Load sky model input
+        M = load_sky_model()
+
+    comm.Bcast([M, MPI.DOUBLE], root=MASTER)
+
+    # Calculate epsilon slice
+    R = load_response_matrix(comm, start_row, end_row)
+    epsilon_slice = np.dot(R, M)      # XXX: Can be GPU accelerated
+
+    # All gather epsilon slices
+    comm.Allgatherv(epsilon_slice, [epsilon, [averow] * numtasks, (np.arange(numtasks) * averow).tolist(), MPI.DOUBLE])
 
     if taskid == 0:
-        print(f"Hello from Master. Starting with {numworkers} worker threads.")
-
-        # Send matrix data to the worker tasks
-        averow = NUMROWS // numworkers
-        extra_rows = NUMROWS % numworkers
-
-        avecol = NUMCOLS // numworkers
-        extra_cols = NUMCOLS % numworkers
-
-        offset_rows = 0
-        offset_cols = 0
-
-        # Task distribution loop
-        for dest in range(1, numworkers + 1):
-            # Load sky model input
-            M = load_sky_model()
-
-            if dest <= extra_rows:
-                rows = averow + 1
-            else:
-                rows = averow
-
-            if dest <= extra_cols:
-                cols = avecol + 1
-            else:
-                cols = avecol
-
-            # Send data to worker
-            mtype = FROM_MASTER
-            comm.send(offset_rows, dest=dest, tag=mtype)
-            comm.send(rows, dest=dest, tag=mtype)
-            comm.send(offset_cols, dest=dest, tag=mtype)
-            comm.send(cols, dest=dest, tag=mtype)
-            comm.send(M, dest=dest, tag=mtype)
-
-            offset_rows = offset_rows + rows
-            offset_cols = offset_cols + cols
-
-        for dest in range(1, numworkers + 1):
-            mtype = FROM_WORKER
-            offset_rows = comm.recv(source=dest, tag=mtype)
-            rows = comm.recv(source=dest, tag=mtype)
-            epsilon[offset_rows : offset_rows + rows] = comm.recv(source=dest, tag=mtype)
-
         print(epsilon)
         print()
 
-    elif taskid > 0:
-        # Receive index range for R and R-transpose
-        mtype = FROM_MASTER  # Message tag
-        offset_rows = comm.recv(source=0, tag=mtype)
-        rows = comm.recv(source=0, tag=mtype)
-        offset_cols = comm.recv(source=0, tag=mtype)
-        cols = comm.recv(source=0, tag=mtype)
-        M = np.empty(NUMCOLS, dtype=np.float64)
-        M = comm.recv(source=0, tag=mtype)
+    # if taskid == MASTER:
+    #     for dest in range(1, numworkers + 1):
+    #         mtype = FROM_WORKER
+    #         offset_cols = comm.recv(source=dest, tag=mtype)
+    #         cols = comm.recv(source=dest, tag=mtype)
+    #         C[offset_cols : offset_cols + cols] = comm.recv(source=dest, tag=mtype)
+    #     print(C)
 
-        # Calculate epsilon slice
-        R = init_response_matrix()
-        for i in range(rows):
-            for j in range(NUMCOLS):
-                epsilon[i] += R[offset_rows + i, j] * M[j]
+    # elif taskid > MASTER:
+    #     # Calculate C slice
+    #     for j in range(cols):
+    #         for i in range(NUMROWS):
+    #             C[j] += R[i, offset_cols + j] / epsilon[i]
 
-        # Send epsilon slice back to master
-        comm.send(offset_rows, dest=0, tag=FROM_WORKER)
-        comm.send(rows, dest=0, tag=FROM_WORKER)
-        comm.send(epsilon[:rows], dest=0, tag=FROM_WORKER)
+    #     # Send C slice back to master
+    #     comm.send(offset_cols, dest=MASTER, tag=FROM_WORKER)
+    #     comm.send(cols, dest=MASTER, tag=FROM_WORKER)
+    #     comm.send(C[:cols], dest=MASTER, tag=FROM_WORKER)
 
-    comm.Bcast(epsilon, root=MASTER)
-
-    if taskid == 0:
-        for dest in range(1, numworkers + 1):
-            mtype = FROM_WORKER
-            offset_cols = comm.recv(source=dest, tag=mtype)
-            cols = comm.recv(source=dest, tag=mtype)
-            C[offset_cols : offset_cols + cols] = comm.recv(source=dest, tag=mtype)
-        print(C)
-
-    elif taskid > 0:
-        # Calculate C slice
-        for j in range(cols):
-            for i in range(NUMROWS):
-                C[j] += R[i, offset_cols + j] / epsilon[i]
-
-        # Send C slice back to master
-        comm.send(offset_cols, dest=0, tag=FROM_WORKER)
-        comm.send(cols, dest=0, tag=FROM_WORKER)
-        comm.send(C[:cols], dest=0, tag=FROM_WORKER)
+    print("Almost Done\n")
+    MPI.Finalize()
+    print("Done")
 
 if __name__ == "__main__":
     main()
